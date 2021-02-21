c19dd32e4a8ad761f3c2f541a5455e5b3a4fcb8a
CharlesXu1124 <xuzheyuan961124@gmail.com>
2021-02-20 20:42:02 -0800

    created flask gateways

--- storeService.py
+++ storeService.py
@@ -12,6 +12,9 @@ from flask import request
 
 import pyodbc
 
+
+app = Flask(__name__)
+
 # helper function for generating random hashes
 def random_string(length):
     pool = string.ascii_uppercase + string.digits
@@ -25,3 +28,28 @@ def random_string_lower_case(length):
     pool = string.ascii_lowercase + string.digits
     return ''.join(random.choice(pool) for i in range(length))
 
+
+
+@app.route('/')
+def index():
+    return 'invalid call'
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+
+@app.route('/searchRestaurant', methods=['GET', 'POST'])
+def searchRestaurant():
+
+
+@app.route('/placeOrder', methods=['GET', 'POST'])
+def placeOrder():
+    
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
